d4904c2d15ab1b48d3894a24edfb1f7804d1716c
Doniyorbek Tokhirov <tokhirovdoniyor@gmail.com>
2023-10-17 13:59:59 +0500

    feat: added max text width of 80 and 100 for go

--- init.lua
+++ init.lua
@@ -5,5 +5,12 @@ autocmd("VimResized", { pattern = "*", command = "tabdo wincmd =", })
 autocmd("BufAdd", { command = "set rnu", })
 autocmd("BufWritePost", { command = "FormatWrite", })
 
+autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = "*.go",
+    callback = function() vim.opt_local.textwidth = 100 end
+})
+
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = "+1"
